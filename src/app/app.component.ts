ea458ab9acd511a40f0e750d749220ff396538c2
rakesh <rakeshraih@gmail.com>
2017-09-03 12:58:43 -0700

    Adding observer on input, click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  scriptName = '';
+  scriptDetail; string;
+  scripts;
+  scriptsLatest = new Subject<string>();
+  resetObser = new Observable;
+  inputCount:number = 10;
+
+  constructor(public http: Http) {
+    this.scripts = this.scriptsLatest
+    .debounceTime(500)
+    .distinct()
+    .filter(scriptName => !!scriptName)
+    .retry(3)
+    .switchMap(
+      scriptName => this.http.get(`http://www.nasdaq.com/aspx/symbolnamesearch.aspx?q=+${scriptName}`,{})
+      .map( res => res.text().split('\n'))
+    ).distinctUntilChanged()
+    .filter(res => true);
+     
+  }
+  ngOnInit(){
+   Observable.fromEvent(document.getElementById("reset-button"), 'click')
+   .debounceTime(500)
+   .subscribe(event => {
+     this.scriptName = '';
+     console.log('clicked');
+    });
+
+    Observable.fromEvent(document.querySelector("input"), 'input')
+    // .debounceTime(500)
+    .subscribe(event => {
+      this.inputCount = this.inputCount - this.scriptName.length;
+      console.log('clicked');
+     });
+
+  }
+
+  changeInScriptName(scriptName) {
+    this.scriptsLatest.next(scriptName);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
